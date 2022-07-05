d246ec60d75fdb192c71285865097d95ce85193d
OrianaToscano <100932557+OrianaToscano@users.noreply.github.com>
2022-07-05 08:43:27 -0300

    Agrega modificaciones
    Estilos pantalla inicial (index) e inicio de fetch

--- pantalla_juego/pantalla_juego.js
+++ pantalla_juego/pantalla_juego.js
@@ -1,26 +1,71 @@
-function mostrarOcultar(id){
-    let cuestionario = document.getElementById('cuestionario');
-    let informacion = document.getElementById('informacion');
-    let final = document.getElementById('final');
-    let container = document.getElementById('container');
+// function mostrarOcultar(id){
+//     let cuestionario = document.getElementById('cuestionario');
+//     let informacion = document.getElementById('informacion');
+//     let final = document.getElementById('final');
+//     let container = document.getElementById('container');
     
-    if(id==='informacion'){
-        informacion.style.display = 'block';
-        cuestionario.style.display = 'none';
-        container.style.display = 'block';
-        final.style.display = 'none';
+//     if(id==='informacion'){
+//         informacion.style.display = 'block';
+//         cuestionario.style.display = 'none';
+//         container.style.display = 'block';
+//         final.style.display = 'none';
 
-    }else if(id==='cuestionario'){
-        informacion.style.display = 'none';
-        cuestionario.style.display = 'block';
-        container.style.display = 'block';
-        final.style.display = 'none';
+//     }else if(id==='cuestionario'){
+//         informacion.style.display = 'none';
+//         cuestionario.style.display = 'block';
+//         container.style.display = 'block';
+//         final.style.display = 'none';
 
-    }else if(id==='final'){
-        informacion.style.display = 'none';
-        cuestionario.style.display = 'none';
-        container.style.display = 'none';
-        final.style.display = 'block';
-    }
+//     }else if(id==='final'){
+//         informacion.style.display = 'none';
+//         cuestionario.style.display = 'none';
+//         container.style.display = 'none';
+//         final.style.display = 'block';
+//     }
+
+// }
+
+
+function carga_preguntas(numeroNivel){
+    const url = `https://proyecto.lara-malenamale.repl.co/listaPreguntas/nivel/${numeroNivel}`
+
+    fetch(url)
+      .then( response => {
+        if (!response.ok) {
+          throw new Error(`HTTP error: ${response.status}`);
+        }
+        return response.text();
+      })
+      .then( texto => {
+		    console.log(texto)
+        const objeto = JSON.parse(texto);
+
+      })
+      .catch( err => console.error(`Fetch problem: ${err.message}`) );
+}
+
+
+function pregunta(){
 
 }
+
+
+
+
+
+
+
+
+
+function crea_lista(nombre_lista, txt) {
+	var select = document.getElementById(nombre_lista);
+	for(var i = 0; i < txt.length; i++) {
+		var opt = txt[i];
+		var el = document.createElement("option");
+		el.textContent = opt;
+		el.value = opt;
+		select.appendChild(el);
+	}
+}
+
+
