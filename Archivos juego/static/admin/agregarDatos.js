f07f0805aa3f017385430f329ffdc3e4d85f0bec
OrianaToscano <orianatt04@gmail.com>
2022-07-13 00:53:33 -0300

    Agrega validacion
    Agrega la validación de datos en el forms para agregar preguntas a la base

--- Archivos juego/static/admin/agregarDatos.js
+++ Archivos juego/static/admin/agregarDatos.js
@@ -15,21 +15,126 @@ function mostrarFormRespuestas(eleccion){
             <div class="row g-3">
                 <div class="col-11">
                     <label for="rta${i}" class="form-label mb-0">Respuesta</label>
-                    <input type="text" class="form-control mt-0 mb-2" id="rta${i}" placeholder="Ingrese la respuesta que quiere agregar">
+                    <input type="text" class="form-control mt-0 mb-2" id="rta${i}" placeholder="Ingrese la respuesta que quiere agregar" required>
                 </div>
                 <div class="col-1 ">
                     <label for="correcto${i}" class="form-label mb-0">Correcta?</label> <br>
-                    <input class="form-check-input h-50 w-50" type="checkbox" id="correcto${i}">
+                    <input class="form-check-input" type="radio" id="correcto${i}" name="correcto" value="correcto${i}" style="height:30px;width:30px;" required>
                 </div>
             </div>
         </div>
         `;
     };
 
-    forms += `<input class="btn btn-primary col-12" type="submit" value="Enviar Nueva Pregunta">`
+    forms += `<input class="btn btn-primary col-12" value="Enviar Nueva Pregunta" type="submit">`
     return document.getElementById('respuestas').innerHTML = forms;
 }
 
 function validar(){
-    
+    let textoPregunta = document.getElementById('textoPregunta').value; 
+    let categoriaPregunta = document.getElementById('categoria').value; 
+    let nivelPregunta = document.getElementById('nivel').value; 
+    // let respuesta1 = document.getElementById('rta1').value;
+    // let respuesta2 = document.getElementById('rta2').value;
+    // let respuesta3 = document.getElementById('rta3').value;
+    // let correcta = document.getElementsByName('correcto');
+
+    let alert = new bootstrap.Modal(document.getElementById('alert'))
+
+    if(textoPregunta.length <= 0){
+        document.getElementById('alertBody').innerHTML = `
+            Falta completar el campo <i>Pregunta</i>. Ingresá el texto de la pregunta que quieras agregar a la base`
+        alert.show()
+    };
+    if(categoriaPregunta == 'null'){
+        document.getElementById('alertBody').innerHTML = `
+            Falta elegir una opcion para el campo <i>Categoria</i>. Seleccioná la categoría correspondiente`
+        alert.show()
+    };
+    if(nivelPregunta == 'null'){
+        document.getElementById('alertBody').innerHTML = `
+        Falta elegir una opcion para el campo <i>Nivel</i>. Seleccioná el nivel correspondiente`
+        alert.show()
+    };
+    if((nivelPregunta == '1' && categoriaPregunta !== 'Agenda 2030') || (nivelPregunta == 'bonus' && categoriaPregunta !== 'Estadísticas')){
+        document.getElementById('alertBody').innerHTML = `
+        Las opciones seleccionadas de <i>Categoría</i> y <i>Nivel</i> no corresponden entre sí. 
+            - Nivel 1 --> Categoría Agenda 2030
+            - Nivel 2 --> Categoría ODS 4, ODS 10, ODS 15
+            - Nivel 3 --> Categoría Estadísticas`
+        alert.show()
+    };
+    console.log('a')
+
 }
+
+// ----------- VALIDACION BOOTSTRAP ------------
+
+(function () {
+    'use strict'
+  
+    // Fetch all the forms we want to apply custom Bootstrap validation styles to
+    var forms = document.querySelectorAll('.needs-validation')
+  
+    // Loop over them and prevent submission
+    Array.prototype.slice.call(forms)
+    .forEach(function (form) {
+        form.addEventListener('submit', function (event) {
+            if (!form.checkValidity()) {
+                event.preventDefault()
+                event.stopPropagation()
+            }
+
+            form.classList.add('was-validated')
+        }, false)
+    })
+    validacionManual();
+})()
+
+// ---------------------------------------------
+
+// ------------- VALIDACION MANUAL -------------
+
+function validacionManual(){
+    let categoriaPregunta = document.getElementById('categoria').value; 
+    let nivelPregunta = document.getElementById('nivel').value;
+    console.log(categoriaPregunta, nivelPregunta)
+
+    let alert = new bootstrap.Modal(document.getElementById('alert'))
+
+    switch(nivelPregunta){
+        case '1':
+            if(categoriaPregunta !== 'Agenda 2030'){
+                document.getElementById('alertBody').innerHTML = `
+                La opción de <i>Categoría</i> y <i>Nivel</i> seleccionadas no son compatibles entre sí. Tenga en cuenta lo siguiente:
+                - Nivel 1 --> Categoría Agenda 2030
+                - Nivel 2 --> Categoría ODS 4, ODS 10, ODS 15
+                - Nivel 3 --> Categoría Estadísticas`
+                alert.show()
+            }
+            break;
+        case '2':
+            if(categoriaPregunta == 'Agenda 2030' || categoriaPregunta == 'Estadísticas'){
+                document.getElementById('alertBody').innerHTML = `
+                La opción de <i>Categoría</i> y <i>Nivel</i> seleccionadas no son compatibles entre sí. Tenga en cuenta lo siguiente:
+                - Nivel 1 --> Categoría Agenda 2030
+                - Nivel 2 --> Categoría ODS 4, ODS 10, ODS 15
+                - Nivel 3 --> Categoría Estadísticas`
+                alert.show()
+            }
+            break;
+        case 'bonus':
+            if(categoriaPregunta !== 'Estadísiticas'){
+                document.getElementById('alertBody').innerHTML = `
+                La opción de <i>Categoría</i> y <i>Nivel</i> seleccionadas no son compatibles entre sí. Tenga en cuenta lo siguiente:<br>
+                - Nivel 1 --> Categoría Agenda 2030 <br>
+                - Nivel 2 --> Categoría ODS 4, ODS 10, ODS 15<br>
+                - Nivel 3 --> Categoría Estadísticas`
+                alert.show()
+            }
+            break;
+               
+    }
+}
+
+// ---------------------------------------------
